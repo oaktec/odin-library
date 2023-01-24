126896c5b09d686b319fc2ced8e26b9ecf4c21ed
oaktec <54672803+oaktec@users.noreply.github.com>
2023-01-24 14:18:08 +0000

    Improve display of books

--- javascript.js
+++ javascript.js
@@ -18,11 +18,35 @@ function addBookToLibrary(title, author, pages, read) {
 
 function printLibrary() {
   myLibrary.forEach((book) => {
-    const newBook = document.createElement("div");
-    newBook.classList.add("card");
-    newBook.textContent = book.info();
-    libraryWrapper.append(newBook);
+    generateBookDiv(book);
   });
+}
+
+function generateBookDiv(book) {
+  const newBook = document.createElement("div");
+  newBook.classList.add("card");
+
+  libraryWrapper.append(newBook);
+
+  const title = document.createElement("p");
+  title.textContent = book.title;
+  title.classList.add("title");
+  newBook.append(title);
+
+  const author = document.createElement("p");
+  author.textContent = book.author;
+  author.classList.add("author");
+  newBook.append(author);
+
+  const pages = document.createElement("p");
+  pages.textContent = `${book.pages} pages`;
+  pages.classList.add("pages");
+  newBook.append(pages);
+
+  const read = document.createElement("p");
+  read.textContent = book.read ? `✅ Read` : "❌ Unread";
+  read.classList.add("read");
+  newBook.append(read);
 }
 
 addBookToLibrary("The Overstory", "Richard Powers", 512, false);
