cd0eb94f8e14b663ff080f8a1330f105f060291e
oaktec <54672803+oaktec@users.noreply.github.com>
2023-01-24 13:55:50 +0000

    Implement book class and addBook func

---
+++ javascript.js
@@ -0,0 +1,15 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+
+  this.info = () =>
+    `${title} by ${author}, ${pages} pages, ${read ? "read" : "not read yet"}`;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  myLibrary.push(new Book(title, author, pages, read));
+}
